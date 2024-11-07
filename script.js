b3b231300c4c389e4d5089c616bafe885a6a24a6
Lucas Cantaruti <devlucascantaruti@gmail.com>
2024-11-07 16:18:29 -0300

    segundo commit

--- script.js
+++ script.js
@@ -16,3 +16,15 @@ $(document).ready(function(){
     });
 
 });
+
+const swiper = new Swiper(".swiper-container", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
