f7a727482d672386e1131e09b8e5f6924d6a53dc
Lucas Cantaruti <devlucascantaruti@gmail.com>
2024-11-08 00:13:37 -0300

    Atualizando meu portfolio

--- script.js
+++ script.js
@@ -8,10 +8,9 @@ $(document).ready(function(){
       new ScrollMagic.Scene({
         triggerElement: this, // Elemento que ativa a cena
         triggerHook: 0.5, // Quando a seção atinge o centro da tela
-        reverse: true, // Não repetir a animação ao rolar para cima
+        reverse: false, // Não repetir a animação ao rolar para cima
       })
         .setClassToggle(this, "fade-in") // Adiciona classe para animação
-        .addIndicators()
         .addTo(controller);
     });
 
